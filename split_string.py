bfebe0c4d77bb1aec60931825bafc6aa24b3cf5b
Mark Draghicescu <markd38@hotmail.com>
2020-05-06 15:34:48 +0200

    Create split_string.py

---
+++ split_string.py
@@ -0,0 +1,21 @@
+# Define a procedure, split_string, that takes two
+# inputs: the string to split and a string containing
+# all of the characters considered separators. The
+# procedure should return a list of strings that break
+# the source string up by the characters in the
+# splitlist.
+
+
+def split_string(source, splitlist):
+    output = []
+    atsplit = True
+    for char in source:
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                output[-1] = output[-1] + char
+    return output
